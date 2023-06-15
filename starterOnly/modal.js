8a4eafeb3af74e16adbb96bf4c197ab9e8b81f98
Bruh <108009787+Blayout@users.noreply.github.com>
2023-06-15 11:19:07 +0200

    issue 2 ok

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -21,13 +21,60 @@ function launchModal() {
 }
 
 
-
 // close modal event
 const modalClose = document.getElementById("close-modal");
 modalClose.addEventListener("click", closeModal);
 
-// close modal form
+
 function closeModal() {
   modalbg.style.display = "none";
 }
+
+
+
+function validate() {
+
+  const firstName = document.getElementById("first").value;
+  const lastName = document.getElementById("last").value;
+  const email = document.getElementById("email").value;
+  const quantity = document.getElementById("quantity").value;
+  const location = document.querySelector("input[name='location']:checked");
+  const checkbox1 = document.getElementById("checkbox1");
+
+
+  if (firstName.trim() === "" || firstName.length < 2) {
+    alert("Veuillez entrer un prénom valide (minimum 2 caractères).");
+    return false;
+  }
+
+  if (lastName.trim() === "" || lastName.length < 2) {
+    alert("Veuillez entrer un nom valide (minimum 2 caractères).");
+    return false;
+  }
+
+  if (email.trim() === "" || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+    alert("Veuillez entrer une adresse email valide.");
+    return false;
+  }
+
+  if (quantity.trim() === "" || isNaN(quantity)) {
+    alert("Veuillez entrer une valeur numérique pour le nombre de concours.");
+    return false;
+  }
+
+  if (!location) {
+    alert("Veuillez sélectionner un tournoi.");
+    return false;
+  }
+
+  if (!checkbox1.checked) {
+    alert("Veuillez accepter les conditions d'utilisation.");
+    return false;
+  }
+
+  return true;
+}
+
+
+
 
